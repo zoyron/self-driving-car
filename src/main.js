659c4c44fd5f92439da135723d835bf6beab70e4
zoyron <sgrarora3@gmail.com>
2023-10-14 20:30:17 +0530

    imported the context object

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
 const canvas = document.getElementById("myCanvas");
 canvas.height = window.innerHeight;
 canvas.width = 250;
+
+/* since we are about to draw cars and other things in 2D in this
+ project, we need to get the context of the canvas. 
+ Context is an object that provides with tools like functions 
+ and other methods to draw lines, shapes, and  other stuff. 
+ For 3d context we use the getContext("webgl") 
+ */
+const ctx = canvas.getContext("2d");
+
+const car = new Car(100, 100, 30, 50); // passing values in the car object
+
