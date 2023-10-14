a9b91c220642533a3fc41232e71fe7a3a8727212
zoyron <sgrarora3@gmail.com>
2023-10-14 20:44:59 +0530

    created the car objec in the car.js file, and then moved the file in the src folder

---
+++ src/car.js
@@ -0,0 +1,22 @@
+// will be defining the car class here
+
+
+/*
+this is the class Car whose object we used in the main.js file
+here x and y are the initial co-ordinates of the car and 
+width and height are understandable variables for the car,
+respectively.
+*/
+class Car {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.widht = width;
+    this.height = height;
+  }
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.rect(this.x - this.widht / 2, this.y - this.height / 2, this.widht, this.height);
+    ctx.fill();
+  }
+}
